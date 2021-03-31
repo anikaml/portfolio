6e4770147602c69edb6bfea8ed62bd8727dd1a84
Anika Mlodzianowski <anikamlodzianowski@gmail.com>
2021-03-31 10:35:24 -0700

    deleted unused code

--- src/containers/projects/F1app.js
+++ src/containers/projects/F1app.js
@@ -11,28 +11,6 @@ import MultiFormatPhoto from "../../components/photos/MultiFormatPhoto";
 import { greyBorderColor } from "../../utils/colors";
 
 const useStyles = makeStyles(() => ({
-  datetteImg: {
-    margin: "1em",
-    maxHeight: "45vh",
-    borderRadius: "1.7em",
-    boxShadow: "0rem 0.6rem 1.3rem 0rem rgba(0,0,0,0.2)",
-    display: 'block',
-    '&:hover': {
-      transition: "all 0.5s ease-in-out",
-      transform: "scale(1.05)",
-      boxShadow: "0.5rem 1rem 2rem 0.5rem rgba(0,0,0,0.2)",
-      cursor: 'pointer',
-    },
-    '@media (max-width:900px)': {
-      maxHeight: "30vh",
-      borderRadius: "1em",
-    },
-     /* IpadPro  Portrait */
-     '@media only screen and (min-width: 1024px) and (max-height: 1366px) and (orientation: portrait) and (-webkit-min-device-pixel-ratio: 1.5)': {
-      borderRadius: '3em',
-      maxHeight: "35vh",
-    },
-  },
   grid: {
     display: "flex",
     flexWrap: "wrap",
