2cdd854a050a15ed7a28ba772241a31248620429
Anika Mlodzianowski <anikamlodzianowski@MotMot.attlocal.net>
2021-05-11 18:06:42 -0700

    updated About page design
    Former-commit-id: 4dded47d63a2a3f182605d81774239a48d424e0b

--- src/components/layout/about/AboutText.js
+++ src/components/layout/about/AboutText.js
@@ -6,7 +6,7 @@ import SVG from 'react-inlinesvg';
 
 // Material UI Components
 import { makeStyles } from '@material-ui/core/styles';
-import { Container, Link, Typography } from '@material-ui/core/';
+import { Grid, Link, Typography } from '@material-ui/core/';
 
 import SocialMediaLink from '../../socialMedia/SocialMediaLink';
 import FadeIn from '../../../containers/style/FadeIn';
@@ -19,31 +19,43 @@ const useStyles = makeStyles((theme) => ({
     '@media (max-width:900px)': {
       padding: "1em 1em 30em 1em",
       height: "auto",
+      marginTop: isTablet ? '0em' : '-3em',
+    },
+    '@media (max-width:600px)': {
+      padding: "1em 1em 20em 1em",
+      height: "auto",
       marginTop: '-3em',
     },
-    '@media (max-width:350px)': {
-      padding: "1em 1em 100vh 1em",
-    },
+    // '@media (max-width:350px)': {
+    //   padding: "1em 1em 100vh 1em",
+    // },
     /* IpadPro  Portrait */
     '@media only screen and (min-width: 1024px) and (max-height: 1366px) and (orientation: portrait) and (-webkit-min-device-pixel-ratio: 1.5)': {
       //marginBottom: "-70vh",
     },
     '@media (min-width:901px) and (max-width:1440px)': {
-      marginTop: '-3em',
+      marginTop: isTablet ? '2em' : '-4em',
     },
     '@media (min-width:1441px)': {
       marginTop: '-2em',
     },
   },
   container: {
-    margin: "2em auto 0em auto",
+    margin: "0em auto",
     height: '100vh',
-    '@media (max-width:900px)': {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 'auto',
+    padding: '2em',
+    '@media (max-width:1000px)': {
       padding: "2em 1em 2em 1em",
+      height: 'auto',
     },
      /* IpadPro  Portrait */
      '@media only screen and (min-width: 1024px) and (max-height: 1366px) and (orientation: portrait) and (-webkit-min-device-pixel-ratio: 1.5)': {
       padding: "7em 3em 3em 3em",
+      height: 'auto',
     }
   },
   typography: {
@@ -63,9 +75,26 @@ const useStyles = makeStyles((theme) => ({
     flexWrap: "wrap",
   },
   description: {
+    fontWeight: 300,
+    fontSize: 'calc(16px + 6 * ((100vw - 320px) / 680))',
     '@media (max-width:900px)': {
       fontSize: '1.25rem',
       fontWeight: 400,
+    },
+  },
+  comp: {
+    maxWidth: "-webkit-fill-available",
+    height: "auto",
+    maxHeight: '60vh',
+    width: '100%',
+    '@media (max-width:900px)': {
+      display: 'none',
+    },
+  },
+  text: {
+    padding: '1em 6em',
+    '@media (max-width:900px)': {
+      padding: '1em',
     },
   }
 }));
@@ -77,45 +106,50 @@ export default function AboutText() {
     <>
       <Parallax y={isMobileOnly? [10, -10] : [20, -20]} className={classes.parallaxFooter} tagOuter="section" styleOuter={{margin: 0}}>
         <div id="about" className={classes.div}>
-          <Container maxWidth="md" className={classes.container}>
-            <FadeIn>
-              <Typography variant={isTablet?"h2":isMobile?"h4":"h2"} className={classes.typography} style={{marginBottom: "0.5em"}}>
-              {"Hi,"}
-            </Typography>
-            </FadeIn>
-            <FadeIn>
-              <Typography variant={isTablet?"h3":isMobile? "h5":"h4"} className={classes.typography}>
-                {"I'm Anika. A front-end developer & web designer, currently working at "} 
-                  <Link
-                    href="https://bokiem.solutions"
-                    style={{ textDecoration: 'none'}}
-                    className={classes.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {"Bokiem Solutions"}
-                  </Link>
-                {" San Diego, CA"}
+          <Grid container maxWidth="md" className={classes.container}>
+            <Grid item lg={4}>
+              <SVG src={process.env.PUBLIC_URL + "/icons/computer2.svg"} className={classes.comp}/>   
+            </Grid>
+            <Grid item lg={8} className={classes.text}>
+              <FadeIn>
+                <Typography variant={isTablet?"h2":isMobile?"h4":"h2"} className={classes.typography} style={{marginBottom: "0.5em"}}>
+                {"Hi,"}
               </Typography>
-            </FadeIn>
-            <FadeIn>
-              <Typography variant={isTablet?"h5":isMobile? "h6":"h5"} style={{margin: "1em auto"}} className={`${classes.typography} ${classes.description}`}>
-                {"Design has been my true passion for as long as I remember. I have started my professional journey working as an Architectural Designer in NYC. After a few years of learning new skills and discovering new possibilities along the way, I've decided to try out a new creative outlet - Web Design. After many hours of learning, creating a handful of projects from scratch, and seeing the infinite possibilities for expression that this field offered, I knew that the next step in my career was to become a web designer & front-end developer."}
-              </Typography>
-            </FadeIn>
-            <div className={classes.socialMediaDiv}>
-              <SocialMediaLink
-                href='https://github.com/anikaniescierewicz'
-                name='GitHub'
-              />
-              <SocialMediaLink
-                href='https://linkedin.com/in/anika-mlodzianowski'
-                name='LinkedIn'
-              />
-            </div>
-          </Container>
+              </FadeIn>
+              <FadeIn>
+                <Typography variant={isTablet?"h3":isMobile? "h5":"h4"} className={classes.typography}>
+                  {"I'm Anika. A front-end developer & web designer, currently working at "} 
+                    <Link
+                      href="https://bokiem.solutions"
+                      style={{ textDecoration: 'none'}}
+                      className={classes.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {"Bokiem Solutions"}
+                    </Link>
+                  {" San Diego, CA"}
+                </Typography>
+              </FadeIn>
+              <FadeIn>
+                <Typography variant={isTablet?"h5":isMobile? "h6":"h5"} style={{margin: "1em auto"}} className={`${classes.typography} ${classes.description}`}>
+                  {"Design has been my true passion for as long as I remember. I have started my professional journey working as an Architectural Designer in NYC. After a few years of learning new skills and discovering new possibilities along the way, I've decided to try out a new creative outlet - Web Design and Development. After many hours of learning, creating a handful of projects from scratch, and seeing the infinite possibilities for expression that this field offered, I knew that the next step in my career was to become a web designer & front-end developer."}
+                </Typography>
+              </FadeIn>
+              <div className={classes.socialMediaDiv}>
+                <SocialMediaLink
+                  href='https://github.com/anikaniescierewicz'
+                  name='GitHub'
+                />
+                <SocialMediaLink
+                  href='https://linkedin.com/in/anika-mlodzianowski'
+                  name='LinkedIn'
+                />
+              </div>
+            </Grid>
+          </Grid>
         </div>
-        <SVG src={process.env.PUBLIC_URL + "/icons/about_wave.svg"} />          
+        <SVG src={process.env.PUBLIC_URL + "/icons/about_wave.svg"} style={{marginTop: "-2px"}}/>          
       </Parallax>
     </>
   )
